c5915b5ad049a4facfba563c4dbbc05a3922dc4e
ツfỈRË <77549673+Fire09@users.noreply.github.com>
2023-08-04 10:12:11 +0100

    j

---
+++ server-data/resources/[WEAPONS]/rd-custom_weapons_melee/fxmanifest.lua
@@ -0,0 +1,35 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+---------------------------------------------------------------------------
+-- INCLUDED FILES
+---------------------------------------------------------------------------
+files {
+	'weaponcomponents.meta',
+	'pedpersonality.meta',
+	'weaponanimations.meta',
+	'weaponarchetypes.meta',
+	'weapon_shiv.meta',
+	'weapon_katanas.meta',
+	'weapon_bats.meta',
+	'weaponmachete.meta',
+	'weaponswitchblade.meta'
+	}
+
+---------------------------------------------------------------------------
+-- DATA FILES
+---------------------------------------------------------------------------
+	data_file 'WEAPONCOMPONENTSINFO_FILE' 'weaponcomponents.meta'
+	data_file 'PED_PERSONALITY_FILE' 'pedpersonality.meta'
+	data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
+	data_file 'WEAPON_METADATA_FILE' 'weaponarchetypes.meta'
+	data_file 'WEAPONINFO_FILE' 'weapon_shiv.meta'
+	data_file 'WEAPONINFO_FILE_PATCH' 'weapon_shiv.meta'
+	data_file 'WEAPONINFO_FILE' 'weapon_katanas.meta'
+	data_file 'WEAPONINFO_FILE_PATCH' 'weapon_katanas.meta'
+	data_file 'WEAPONINFO_FILE' 'weapon_bats.meta'
+	data_file 'WEAPONINFO_FILE_PATCH' 'weapon_bats.meta'
+	data_file 'WEAPONINFO_FILE' 'weaponmachete.meta'
+	data_file 'WEAPONINFO_FILE_PATCH' 'weaponmachete.meta'
+	data_file 'WEAPONINFO_FILE' 'weaponswitchblade.meta'
+	data_file 'WEAPONINFO_FILE_PATCH' 'weaponswitchblade.meta'
