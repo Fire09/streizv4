a2dbf5c3579b546b0bff5eadeb23bd98a003af8e
ツfỈRË <77549673+Fire09@users.noreply.github.com>
2023-08-04 13:24:09 +0100

    j

---
+++ server-data/resources/[SONGS]/nikez-occlusion/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'Nikez'
+
+files {
+    'nopixel_mix.dat15.rel',
+    'nopixel_game.dat151.rel'
+}
+
+data_file 'AUDIO_DYNAMIXDATA' 'nopixel_mix.dat'
+data_file 'AUDIO_GAMEDATA' 'nopixel_game.dat'
