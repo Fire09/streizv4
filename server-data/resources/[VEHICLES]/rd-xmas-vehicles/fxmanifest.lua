eef9942e11bc4faa37860a47cea0a124469bc918
ツfỈRË <77549673+Fire09@users.noreply.github.com>
2023-08-04 10:09:33 +0100

    k

---
+++ server-data/resources/[VEHICLES]/rd-xmas-vehicles/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+files {
+	'data/carvariations.meta',
+	'data/handling.meta',
+	'data/vehicles.meta',
+}
+
+data_file 'HANDLING_FILE'			'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'	'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE'	'data/carvariations.meta'
---
+++ server-data/resources/[VEHICLES]/veh_lykan/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version "cerulean"
+
+games {"gta5"}
+
+description "Lykan"
+
+files {
+	"meta/handling.meta",
+	"meta/vehicles.meta",
+	"meta/carcols.meta",
+	"meta/carvariations.meta",
+	"meta/vehiclelayouts.meta",
+	"meta/caraddoncontentunlocks.meta"
+}
+
+data_file "HANDLING_FILE" "meta/handling.meta"
+
+data_file "VEHICLE_METADATA_FILE" "meta/vehicles.meta"
+
+data_file "CARCOLS_FILE" "meta/carcols.meta"
+
+data_file "VEHICLE_VARIATION_FILE" "meta/carvariations.meta"
+
+data_file "VEHICLE_LAYOUTS_FILE" "meta/vehiclelayouts.meta"
+
+data_file "CONTENT_UNLOCKING_META_FILE" "meta/caraddoncontentunlocks.meta"
